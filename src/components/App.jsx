ecb374e337ba00b71c477cd19b3114e938b86623
Alexander Belov <marlo-nn@yandex.ru>
2023-05-13 08:17:09 +0300

    feat__template

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,14 @@
+import React from 'react';
+
 import './App.css';
+import Header from './Header';
+import Dashboard from './Dashboard'
+
 
 function App() {
   return (
     <div className="App">
-
+      <Header />
     </div>
   );
 }
