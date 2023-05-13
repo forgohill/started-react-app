83e2e0edf3d73e5bb67586ca722150f54c317c41
Alexander Belov <marlo-nn@yandex.ru>
2023-05-13 08:41:51 +0300

    создали роут дашборд

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import './App.css';
 import Header from './Header';
+import { Route, Routes } from 'react-router-dom'; // импортируем Routes
 import Dashboard from './Dashboard'
 
 
@@ -9,6 +10,10 @@ function App() {
   return (
     <div className="App">
       <Header />
+
+      <Routes>
+        <Route path='/' element={<Dashboard />} />
+      </Routes>
     </div>
   );
 }
