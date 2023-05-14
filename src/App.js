1cf80ece74af0978d14bfd657078924de0780239
Alexander Belov <marlo-nn@yandex.ru>
2023-05-14 15:32:21 +0300

    import__app

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+import Doubler from './Doubler';
+import { BrowserRouter, Route, Link, Routes } from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <h1>Маршрутный удвоитель</h1>
+        <p>Чтобы запустить удвоитель, введите маршрут. Допустимы только URL-адреса, состоящие из цифр!</p>
+        <Link to="/2">2</Link>
+        <Link to="/10">10</Link>
+        <Link to="/42">42</Link>
+        <Link to="/coffee">Coffee</Link>
+        <Routes>
+          <Route path="/:number" element={<Doubler />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './components/App';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+      <App />
   </React.StrictMode>
 );
-
-// Если вы хотите начать измерение производительности в своем приложении, передайте функцию
-// для регистрации результатов (например: reportWebVitals(console.log))
-// или отправить на конечную точку аналитики. Узнайте больше: https://bit.ly/CRA-vitals
-
