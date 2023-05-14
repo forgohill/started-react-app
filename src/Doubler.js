ae2153c136a77af5ec71561d2cc61077c0d72728
Alexander Belov <marlo-nn@yandex.ru>
2023-05-14 16:25:38 +0300

    удвоитель сделан

--- src/Doubler.js
+++ src/Doubler.js
@@ -1,12 +1,38 @@
 import React from 'react';
+import { useParams } from 'react-router-dom'
+
 import './App.css';
 
-function Doubler() {
-  return (
-    <>
-      <p>{/* сюда должно попасть значение number */}</p>
-    </>
-  );
+
+function Doubler(props) {
+
+  const { number } = useParams();
+
+  React
+    .useEffect(() => {
+      console.log({ number });
+      console.log(number * 2);
+      console.log(Number.isFinite(number * 2))
+
+      return () => {
+      };
+    }, []);
+
+
+  if (!Number.isFinite(number * 2)) {
+
+    return (
+      <p>НЕЕЕТ, ЭТО НЕ ЧИСЛО</p>
+    );
+  } else {
+
+    return (
+      <>
+        <p>{number * 2}</p>
+      </>
+    );
+  }
+
 }
 
 export default Doubler;
