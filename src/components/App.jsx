60a80d305ea791107599c19240e7f542c03248fb
Alexander Belov <marlo-nn@yandex.ru>
2023-06-02 21:40:00 +0300

    sec__commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,37 @@
 import React from 'react';
+import { Route, Routes, Switch, Navigate } from 'react-router-dom'
 
+import Login from './Login.jsx';
+import Register from './Register.jsx';
+import Ducks from './Ducks.jsx';
+import MyProfile from './MyProfile.jsx';
+
+// импорт CSS
 import './App.css';
-import Header from './Header';
-import Dashboard from './Dashboard'
-
 
 function App() {
+  const [loggedIn, setloggedIn] = React.useState(false);
+
   return (
-    <div className="App">
-      <Header />
-    </div>
+    <Routes>
+      <Route path="/" element={
+        loggedIn
+          ? <Navigate to="/ducks" replace />
+          : <Navigate to="/login" replace />}
+      />
+      <Route path='/ducks' element={<Ducks />} />
+      <Route path='/my-profile' element={<MyProfile />} />
+      <Route path='/login' element={
+        <div className='loginContaner'>
+          <Login />
+        </div>
+      } />
+      <Route path='/register' element={
+        <div className='registerContaner'>
+          < Register />
+        </div>
+      } />
+    </Routes>
   );
 }
 
---
+++ src/components/Ducks.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Ducks() {
+  return (<div>Ducks</div>);
+}
+
+export default Ducks;
